dd30acdcb7ddf976b66cb4ca9f244477770f4d61
Edgar Chirivella <edgar.95.12@gmail.com>
2021-12-29 20:31:41 +0100

    Add migration to create Vehicles table

---
+++ VehiclesTest/VehiclesTest/Migrations/20211229193007_CreateVehicles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehiclesTest.Migrations
+{
+    public partial class CreateVehicles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OrderNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    Vin = table.Column<string>(type: "TEXT", nullable: false),
+                    Model = table.Column<string>(type: "TEXT", nullable: false),
+                    LicencePlate = table.Column<string>(type: "TEXT", nullable: false),
+                    DeliveryDate = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
