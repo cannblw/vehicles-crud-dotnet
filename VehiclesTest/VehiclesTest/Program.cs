bf98a1d9009b0566d8ee4345c883ba1ea7a9d92c
Edgar Chirivella <edgar.95.12@gmail.com>
2021-12-29 19:34:01 +0100

    Override convention to have lowercase URLs

--- VehiclesTest/VehiclesTest/Program.cs
+++ VehiclesTest/VehiclesTest/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRouting(opt => opt.LowercaseUrls = true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
