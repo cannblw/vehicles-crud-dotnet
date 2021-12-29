ef29f3a7ba40807fe4ee575623acfee6cf764c01
Edgar Chirivella <edgar.95.12@gmail.com>
2021-12-29 20:31:21 +0100

    Add Vehicle model to DbContext

--- VehiclesTest/VehiclesTest/Database/AppDbContext.cs
+++ VehiclesTest/VehiclesTest/Database/AppDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using VehiclesTest.Models;
 
 namespace VehiclesTest.Database
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Vehicle> Vehicles { get; set; }
+        
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
--- VehiclesTest/VehiclesTest/Models/Vehicle.cs
+++ VehiclesTest/VehiclesTest/Models/Vehicle.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace VehiclesTest.Models
 {
-    public class Vehicle
+    public class Vehicle : Entity
     {
+        public string OrderNumber { get; set; }
+
+        public string Vin { get; set; }
         
+        public string Model { get; set; }
+        
+        public string LicencePlate { get; set; }
+        
+        public DateTimeOffset DeliveryDate { get; set; }
     }
 }
